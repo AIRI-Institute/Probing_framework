d481fa982289a467724ee8787333e2ccdd08934d
Vitaly Protasov <izakladno@yandex.ru>
2022-02-03 14:46:21 +0300

    added func

--- probing/utils.py
+++ probing/utils.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Optional
+from typing import Optional, List, Union
 from transformers import  AutoConfig, AutoModel, AutoTokenizer
 import torch
 
@@ -18,8 +18,26 @@ class TransformerUtilities:
         self.tokenizer = AutoTokenizer.from_pretrained(model_name, config=self.config)
 
         if device:
-            self.model.to(torch.device(device))
+            self.device = device
+            self.model.to(torch.device(self.device))
         elif torch.cuda.is_available():
             self.model.cuda()
+            self.device = self.model.device
         else:
-            self.model.to(torch.device("cpu"))
+            self.device = "cpu"
+            self.model.to(torch.device(self.device))
+
+    def encode_text(self, text: Union[str, List[str], List[List[str]]]):
+        encoded_text = self.tokenizer(text, padding="longest", return_tensors="pt")
+        input_ids = encoded_text["input_ids"].to(self.device)
+        attention_mask = encoded_text["attention_mask"].to(self.device)
+        with torch.no_grad():
+            model_outputs = self.model(
+                    input_ids, attention_mask, return_dict=True
+            )
+            model_outputs = (
+                model_outputs["hidden_states"]
+                if "hidden_states" in model_outputs
+                else model_outputs["encoder_hidden_states"]
+            )
+            return model_outputs
