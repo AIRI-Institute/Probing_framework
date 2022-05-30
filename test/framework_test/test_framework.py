1f4638649efbb2ea6288b94fa2c11f71d421ce14
Vitaly Protasov <izakladno@yandex.ru>
2022-05-30 15:17:30 +0300

    add testing file

---
+++ test/framework_test/test_framework.py
@@ -0,0 +1,19 @@
+import pytest
+import unittest
+from pathlib import Path
+
+from probing.data_former import DataFormer
+from probing.config import data_folder
+
+
+@pytest.mark.data_former
+class TestDataFormer(unittest.TestCase):
+
+    def test1(self):
+        task_name = 'sent_len'
+        data = DataFormer(
+                probe_task=task_name
+            )
+        task_path = Path(data.data_path)
+        self.assertEqual(task_path.parent, data_folder)
+        self.assertEqual(task_path, Path(data_folder, f'{task_name}.txt'))
