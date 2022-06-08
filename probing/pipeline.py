208e9dfb9498e055344f3e70163c3eeab835f31a
Vitaly Protasov <izakladno@yandex.ru>
2022-06-09 00:56:52 +0300

    deleted extra pring

--- probing/pipeline.py
+++ probing/pipeline.py
@@ -149,7 +149,6 @@ class ProbingPipeline:
 
         if verbose:
             print(f'Task in progress: {probe_task}\nPath to data: {path_to_file_for_probing}')
-            print("Data encoding.")
 
         encode_func =  lambda x: self.transformer_model.encode_text(x, self.embedding_type)
         train = EncodeLoader(task_dataset["tr"], encode_func, self.batch_size)
