c208300e3014eddf883bf47740b0788f44930e4a
Vitaly Protasov <izakladno@yandex.ru>
2022-06-15 22:20:24 +0300

    added shuffling to data

--- probing/data_former.py
+++ probing/data_former.py
@@ -5,6 +5,7 @@ from tqdm.notebook import tqdm
 from torch.utils.data import DataLoader
 from torch.utils.data import BatchSampler
 import torch
+import numpy as np
 from sklearn import preprocessing
 
 from probing.utils import get_probe_task_path, exclude_rows
@@ -14,14 +15,15 @@ class DataFormer:
     def __init__(
         self,
         probe_task: Union[Enum, str],
-        data_path: Optional[os.PathLike] = None
+        data_path: Optional[os.PathLike] = None,
+        shuffle: bool = True
     ):
         self.probe_task = probe_task
-        self.labels = []
+        self.shuffle = shuffle
         self.data_path = get_probe_task_path(probe_task, data_path)
 
-        self.samples = self.__form_data()
-        self.num_classes = len(self.labels)
+        self.samples, self.unique_labels = self.__form_data()
+        self.num_classes = len(self.unique_labels)
 
     def __len__(self):
         return len(self.samples)
@@ -31,15 +33,19 @@ class DataFormer:
 
     def __form_data(self) -> Dict[Enum, Tuple[Enum, str]]:
         samples_dict = {}
+        unique_labels = []
         f = open(self.data_path)
         for line in list(f):
             data_type, label, text = line.strip().split("\t")
             if data_type not in samples_dict:
                 samples_dict[data_type] = []
             samples_dict[data_type].append((text, label))
-            if label not in self.labels:
-                self.labels.append(label)
-        return samples_dict
+            if label not in unique_labels:
+                unique_labels.append(label)
+
+        if self.shuffle:
+            samples_dict = {k: np.random.permutation(v) for k, v in samples_dict.items()}
+        return samples_dict, unique_labels
 
 
 class EncodeLoader:
@@ -47,9 +53,9 @@ class EncodeLoader:
         self,
         list_texts_labels: List[Tuple[str, Enum]],
         encode_func,
-        batch_size: int=128,
-        drop_last: bool=False,
-        shuffle: bool=True
+        batch_size: int = 128,
+        drop_last: bool = False,
+        shuffle: bool = True
     ):  
         self.encode_func = encode_func
         self.list_texts_labels = list_texts_labels
