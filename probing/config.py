4bf94261eb762a28dd642700d3260f8ea61ee368
Vitaly Protasov <izakladno@yandex.ru>
2022-07-28 14:09:36 +0300

    fixed path res

--- probing/config.py
+++ probing/config.py
@@ -6,5 +6,4 @@ home_path = Path(__file__).resolve().parent.parent
 data_folder = Path(home_path, "data/")
 
 date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
-res_name = f"results/{date}/"
-results_folder = Path(home_path, res_name)
+results_folder = Path(home_path, f"results", f"experiment_{date}")
