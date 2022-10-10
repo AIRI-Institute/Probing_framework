0cd5afa31e59d3c688eba1d00af1bcd3ffcd0b70
Vitaly Protasov <izakladno@yandex.ru>
2022-10-10 16:09:46 +0300

    added test

--- tests/framework_test/test_framework.py
+++ tests/framework_test/test_framework.py
@@ -24,3 +24,14 @@ class TestTextFormer(unittest.TestCase):
             train_epochs=2,
         )
         return True
+
+    def test_launch2(self):
+        task_name = "test_gapping"
+        self.experiment.run(
+            probe_task=task_name,
+            path_to_task_file=self.path_testfile,
+            verbose=True,
+            train_epochs=2,
+            do_control_task = True
+        )
+        return True
