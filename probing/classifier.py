d1e7ce5a806794e406f1b572b8eedfcdc1c21e9a
Ekaterina Voloshina <42929264+EkaterinaVoloshina@users.noreply.github.com>
2022-10-14 14:30:00 +0300

    fixed code style

--- probing/classifier.py
+++ probing/classifier.py
@@ -40,7 +40,14 @@ class MLP(torch.nn.Module):
         return x
     
 class LinearVariational(torch.nn.Module):
-    def __init__(self, in_features: int, out_features: int, parent, bias: bool=True, device: torch.device = torch.device('cpu')) -> None:
+    def __init__(
+        self, 
+        in_features: int, 
+        out_features: int, 
+        parent, 
+        bias: bool=True, 
+        device: torch.device = torch.device('cpu'),
+    ) -> None:
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -49,11 +56,21 @@ class LinearVariational(torch.nn.Module):
         self.parent = parent
         
         if getattr(parent, 'accumulated_kl_div', None) is None:
-            if getattr(parent.parent, 'accumulated_kl_div', None) is None: parent.accumulated_kl_div = 0
-            else: parent.accumulated_kl_div = parent.parent.accumulated_kl_div
+            if getattr(parent.parent, 'accumulated_kl_div', None) is None: 
+                parent.accumulated_kl_div = 0
+            else: 
+                parent.accumulated_kl_div = parent.parent.accumulated_kl_div
             
-        self.w_mu = torch.nn.Parameter(torch.FloatTensor(in_features, out_features).normal_(mean = 0, std = 0.001).to(self.device))
-        self.w_p = torch.nn.Parameter(torch.FloatTensor(in_features, out_features).normal_(mean = 0, std = 0.001).to(self.device))
+        self.w_mu = torch.nn.Parameter(
+            torch.FloatTensor(in_features, out_features)
+            .normal_(mean = 0, std = 0.001)
+            .to(self.device)
+        )
+        self.w_p = torch.nn.Parameter(
+            torch.FloatTensor(in_features, out_features)
+            .normal_(mean = 0, std = 0.001)
+            .to(self.device)
+        )
 
         if self.include_bias:
             self.b_mu = torch.nn.Parameter(torch.zeros(out_features))
@@ -67,20 +84,27 @@ class LinearVariational(torch.nn.Module):
     def forward(self, x):
         w = self._reparameterize(self.w_mu, self.w_p)
         
-        if self.include_bias: b = self._reparameterize(self.b_mu, self.b_p)
-        else: b = 0
+        if self.include_bias: 
+            b = self._reparameterize(self.b_mu, self.b_p)
+        else: 
+            b = 0
             
-        z = torch.matmul(x,w) + b
+        z = torch.matmul(x, w) + b
         
         self.parent.accumulated_kl_div += kl_divergence(w, self.w_mu, self.w_p).item()
-        if self.include_bias: self.parent.accumulated_kl_div += kl_divergence(b, self.b_mu, self.b_p).item()
+        if self.include_bias: 
+            self.parent.accumulated_kl_div += kl_divergence(
+                b, self.b_mu, self.b_p)
+            .item()
         return z
 
 class MDLLinearModel(torch.nn.Module):
     def __init__(self, input_dim: int, hidden_size: int, num_classes: int, device: torch.device = torch.device('cpu')) -> None:
         super().__init__()
         self.kl_loss = KL
-        self.layers = torch.nn.Sequential(LinearVariational(input_dim, num_classes, self.kl_loss, device))
+        self.layers = torch.nn.Sequential(
+            LinearVariational(input_dim, num_classes, self.kl_loss, device)
+        )
 
     @property
     def accumulated_kl_div(self):
