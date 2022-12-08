ec1f51dadf3d8ee710d8f91f6b6e72f55ec70285
Vitaly Protasov <izakladno@yandex.ru>
2022-12-08 18:28:33 +0300

    fixed black

--- probing/encoder.py
+++ probing/encoder.py
@@ -105,18 +105,14 @@ class TransformersLoader:
         self, input_ids_new: torch.Tensor, model_output_tensors_new: torch.Tensor
     ) -> None:
         for input_ids, out_cache_tensor in zip(input_ids_new, model_output_tensors_new):
-            input_ids_unpad = (
-                input_ids
-            )
+            input_ids_unpad = input_ids
             decoded_text = self.tokenizer.decode(input_ids_unpad)
             self.cache[decoded_text] = torch.unsqueeze(out_cache_tensor, 0)
 
     def get_from_cache(self, input_ids_cached: torch.Tensor) -> List[torch.Tensor]:
         cached_tensors_list = []
         for input_ids in input_ids_cached:
-            input_ids_unpad = (
-                input_ids
-            )
+            input_ids_unpad = input_ids
             decoded_text = self.tokenizer.decode(input_ids_unpad)
             cached_tensors_list.append(self.cache[decoded_text])
         return cached_tensors_list
