0d87aba07d75d0eee8501d75d4f0c71fa03b3ae9
Vitaly Protasov <izakladno@yandex.ru>
2022-12-08 13:41:00 +0300

    added test for row excluding funcs

---
+++ tests/framework_test/test_exclude_sentences.py
@@ -0,0 +1,37 @@
+import unittest
+from pathlib import Path
+
+import pytest
+
+from probing.data_former import TextFormer
+from probing.pipeline import ProbingPipeline
+
+
+@pytest.mark.pipeline
+class TestTruncation(unittest.TestCase):
+    path_testfile = Path(Path(__file__).parent.resolve(), "test_cs_cac_AdpType.csv")
+    task_data = TextFormer(Path(path_testfile).stem, path_testfile)
+    task_dataset, num_classes = task_data.samples, len(task_data.unique_labels)
+
+    experiment1 = ProbingPipeline(
+        hf_model_name="bert-base-multilingual-uncased", device="cpu"
+    )
+    experiment2 = ProbingPipeline(hf_model_name="bigscience/bloom-1b7", device="cpu")
+
+    def test_launch_right(self):
+        tokenized_text = self.experiment1.transformer_model.tokenize_text(
+            list(self.task_dataset["tr"][:, 0])
+        )
+        _, _, excluded_rows = self.experiment1.transformer_model._fix_tokenized_tensors(
+            tokenized_text
+        )
+        self.assertEqual(len(excluded_rows), 1)
+
+    def test_launch_left(self):
+        tokenized_text = self.experiment2.transformer_model.tokenize_text(
+            list(self.task_dataset["tr"][:, 0])
+        )
+        _, _, excluded_rows = self.experiment2.transformer_model._fix_tokenized_tensors(
+            tokenized_text
+        )
+        self.assertEqual(len(excluded_rows), 4)
