1b2741c2dc6512f996b1115613d70f5e6d8a2787
Vitaly Protasov <izakladno@yandex.ru>
2023-02-27 13:29:38 +0300

    pylint fix

--- probing/config.py
+++ probing/config.py
@@ -5,5 +5,5 @@ home_path = Path(__file__).resolve().parent.parent
 data_folder = Path(home_path, "data/")
 
 date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
-main_result_folder = "results"
-results_folder = Path(home_path, main_result_folder, f"experiment_{date}")
+MAIN_RES_FOLDER = "results"
+results_folder = Path(home_path, MAIN_RES_FOLDER, f"experiment_{date}")
