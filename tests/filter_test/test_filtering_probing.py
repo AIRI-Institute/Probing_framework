48f4a0fed7c24bf2f87e94c6ec3b854bbd7e9115
Tasia <tasia@Tasias-MacBook-Air.local>
2023-04-05 22:25:26 +0300

    Fixed(2) test_filering_probing.py for pytest

--- tests/filter_test/test_filtering_probing.py
+++ tests/filter_test/test_filtering_probing.py
@@ -25,7 +25,10 @@ class TestProbingConlluFilter(unittest.TestCase):
             "ACL": ({"H": {}, "CL": {}}, {("H", "CL"): {"deprels": "^acl.*?$"}}),
         }
 
-        self.dir_conllu_path = "test_filter_probing_data/conllu_folder/recl"
+        self.dir_conllu_path = Path(
+            Path(__file__).parent.resolve(),
+            "test_filter_probing_data/conllu_folder/recl",
+        )
 
     def test__filter_conllu_all_found(self):
         self.probing_filter.sentences = self.trees_testfile
@@ -70,9 +73,12 @@ class TestProbingConlluFilter(unittest.TestCase):
             self.probing_filter._filter_conllu("by_passive")[0]
 
     def test_upload_files_conllu_paths(self):
-        conllu_paths = [
+        conllu_paths_raw = [
             "test_filter_probing_data/conllu_folder/recl/ru_taiga-ud-test1.conllu",
             "test_filter_probing_data/conllu_folder/recl/ru_taiga-ud-test2.conllu",
+        ]
+        conllu_paths = [
+            str(Path(Path(__file__).parent.resolve(), p)) for p in conllu_paths_raw
         ]
         self.probing_filter.upload_files(conllu_paths=conllu_paths)
         self.assertEqual("ru_taiga", self.probing_filter.language)
@@ -84,12 +90,17 @@ class TestProbingConlluFilter(unittest.TestCase):
         self.assertEqual(32, len(self.probing_filter.sentences))
 
     def test_upload_files_empty_folder(self):
-        empty_path = "test_filter_probing_data/empty_folder"
+        empty_path = Path(
+            Path(__file__).parent.resolve(), "test_filter_probing_data/empty_folder"
+        )
         with self.assertRaises(AssertionError):
             self.probing_filter.upload_files(dir_conllu_path=empty_path)
 
     def test_upload_files_not_conllu_folder(self):
-        not_conllu_path = "test_filter_probing_data/not_conllu_folder"
+        not_conllu_path = Path(
+            Path(__file__).parent.resolve(),
+            "test_filter_probing_data/not_conllu_folder",
+        )
         with self.assertRaises(AssertionError):
             self.probing_filter.upload_files(dir_conllu_path=not_conllu_path)
 
@@ -146,7 +157,10 @@ class TestProbingConlluFilter(unittest.TestCase):
 
     def test_filter_and_convert_too_few_sentences(self):
         self.probing_filter.upload_files(
-            dir_conllu_path="test_filter_probing_data/conllu_folder/recl_too_few"
+            dir_conllu_path=Path(
+                Path(__file__).parent.resolve(),
+                "test_filter_probing_data/conllu_folder/recl_too_few",
+            )
         )
         with self.assertRaises(Exception):
             self.probing_filter.filter_and_convert(queries=self.queries, task_name="cl")
