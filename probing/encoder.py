d85e06723615c774feff80b63ccc0bc3324b2a74
Yuliy Daniel <yulik.daniel@gmail.com>
2023-09-12 22:06:07 +0200

    Added translation from word indices to token indices

--- probing/encoder.py
+++ probing/encoder.py
@@ -167,8 +167,10 @@ class TransformersLoader:
         for output in model_outputs[1:]:  # type: ignore
             if word_level:
                 offset = torch.arange(0, output.size(0) * output.size(1), output.size(1))
-                broadcasted_indices = word_indices + offset.unsqueeze(1)
-                sent_vector = output.reshape(-1, output.shape[-1])[broadcasted_indices].reshape(output.shape[0], -1)
+                broadcasted_indices = word_indices + offset.unsqueeze(1).unsqueeze(1)
+                output[:,0,:] = 0 # The 0th token in each sentence is the CLS token, it doesn't matter to us,
+                                  # and 0 in word_indices means no token, so we should ignore it
+                sent_vector = output.reshape(-1, output.shape[-1])[broadcasted_indices].sum(axis=2).reshape(output.shape[0], -1)
                 # print(output.size(), word_indices.size(), sent_vector.size())
                 # print(output[:, 0, :].size())
 
@@ -193,6 +195,39 @@ class TransformersLoader:
                     )
             layers_outputs.append(sent_vector)
         return layers_outputs
+    
+    def get_token_ids(self, ud_tokenization: List[str], model_tokenization: List[str], word_indices: List[int]
+    ) -> List[List[int]]:
+        mapping = []
+        ind_model = 1 # Skipping [CLS]
+
+        def get_model_token():
+            token = model_tokenization[ind_model]
+            if token.startswith("##"):
+                return token[2:]
+            return token
+
+        for ind in range(len(ud_tokenization)):
+            mapping.append([])
+            char_num = 0
+            while char_num < len(ud_tokenization[ind]):
+                model_token = get_model_token().lower()
+                if ud_tokenization[ind][char_num:char_num+len(model_token)].lower() == model_token:
+                    char_num += len(model_token)
+                    ind_model += 1
+                    mapping[-1].append(ind_model)
+                else:
+                    print("Could not match tokenizations")
+                    print(ud_tokenization)
+                    print(model_tokenization)
+                    print("at index", ind)
+                    raise "Could not match tokenizations"
+        
+        result = []
+        for i in word_indices:
+            result.append(mapping[i])
+        
+        return result
 
     def get_tokenized_datasets(
         self, task_dataset: Dict[Literal["tr", "va", "te"], List[Tuple[str, str, List[int]]]]
@@ -238,6 +273,21 @@ class TransformersLoader:
                 logger.warning(
                     f"Since you decided not to truncate long sentences, {len(row_ids_to_exclude)} sample(s) were excluded"
                 )
+            
+            word_indices_tensor = self.exclude_rows(torch.LongTensor(word_indices), row_ids_to_exclude)
+            sentences_array = np.delete(stage_data[:, 0], row_ids_to_exclude)
+            resulting_inds = []
+            maxlen = 0
+            for ind in range(sentences_array.shape[0]):
+                ids = self.get_token_ids(sentences_array[ind].split(" "),
+                                   self.tokenizer.convert_ids_to_tokens(tokenized_text["input_ids"][ind]),
+                                   word_indices_tensor[ind])
+                resulting_inds.append(ids)
+                maxlen = max(maxlen, max([len(word_tokens) for word_tokens in ids]))
+            
+            # All data has to be a homogenous 2d array to be able to be passed through dataloaders, so we will have to add zeroes
+            for inds in resulting_inds:
+                inds = [word_tokens.extend([0 for _ in range(maxlen - len(word_tokens))]) for word_tokens in inds]
 
             stage_data_dict = TokenizedVectorFormer(
                 {
@@ -246,9 +296,7 @@ class TransformersLoader:
                     "labels": self.exclude_rows(
                         torch.tensor(labels), row_ids_to_exclude
                     ).view(-1),
-                    "word_indices": self.exclude_rows(
-                        torch.LongTensor(word_indices), row_ids_to_exclude
-                    ),
+                    "word_indices": torch.LongTensor(resulting_inds),
                 }
             )
             thing = torch.LongTensor(word_indices)
